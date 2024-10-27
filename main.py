fe0c530e162c96d5e29712e265d667c80d087722
Sniffle <tourach23@gmail.com>
2024-10-26 20:33:59 -0500

    Add player and player motion

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-    ticks = pygame.time.Clock()
+    clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable =  pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        
+        for obj in updatable:
+            obj.update(dt)
         screen.fill("black")
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
-        delta = ticks.tick(60)
-        dt = delta/1000
 
-
+        #framerate limter 60fps
+        dt = clock.tick(60) / 1000
+     
 
 if __name__ == "__main__":
     main()
